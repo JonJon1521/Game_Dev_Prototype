9ff94936fc1ecf0d8f5b15af8d409e89e70af459
AngeliqueHannagan_Game_Dev <ange.hannagan@gmail.com>
2026-03-13 15:59:57 -0400

    added shack to IDamage so the camera will shack when hit

--- Game_Dev_Prototype/Assets/Scripts/IDamage.cs
+++ Game_Dev_Prototype/Assets/Scripts/IDamage.cs
@@ -3,4 +3,8 @@ using UnityEngine;
 public interface IDamage
 {
   void takeDamage(int amount);
+
+    // Shack for 0.15 seconds with the magnitud of 0.2
+
+    GetComponentInChildren<cameraController>().shack(0.15f, 0.2f)
 }
