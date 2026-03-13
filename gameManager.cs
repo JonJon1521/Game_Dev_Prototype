d097fccb6ec08f81200f8dcf97eb28b826d458c7
DJCool360 <fuzzyhead5555@gmail.com>
2026-03-13 14:24:59 -0400

    Add files via upload

---
+++ gameManager.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class gamemanager : MonoBehaviour
+{
+  public static gamemanager instance;
+
+  [SerializeField] GameObject menuActive;
+  [SerializeField] GameObject menuPause;
+  [SerializeField] GameObject menuWin;
+  [SerializeField] GameObject menuLose;
+
+  [SerializeField] TMP_Text gameGoalCountText;
+
+  public Image playerHPBar;
+
+  public GameObject playerDamageFlash;
+
+  public GameObject player;
+  public playerController playerScript;
+  public bool isPaused;
+
+  float timeScaleOrgi;
+
+  int gameGoalCount;
+
+  void Awake()
+  {
+    instance = this;
+    timeScaleOrgi = Time.timeScale;
+
+    player = GameObject.FindWithTag("Player");
+    playerScript = player.GetComponent<playerController>();
+    //playerSpawnPos = GameObject.FindWithTag("Player Spawn Pos");
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetButtonDown("Cancel"))
+        if (menuActive == null)
+          {
+            statePause();
+            menuActive = menuPause;
+            menuActive.SetActive(true);
+          }
+        else if (menuActive == menuPause)
+          {
+            stateUnpause();
+          }
+    }
+
+  public void statePause()
+  {
+    isPaused = true;
+    Time.timeScale = 0;
+    Cursor.visible = true;
+    Cursor.lockState = CursorLockMode.None;
+  }
+
+  public void stateUnpause()
+  {
+    isPaused = false;
+    Time.timeScale = timeScaleOrgi;
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+    menuActive.SetActive(false);
+    menuActive = null;
+  }
+
+  public void updateGameGoal(int amount)
+  {
+    gameGoalCount += amount;
+    gameGoalCountText.text = gameGoalCount.ToString("F0");
+    if (gameGoalCount <= 0)
+     {
+       //you win!!
+       statePause();
+       menuActive = menuWin;
+       menuActive.SetActive(true);
+      }
+    }
+
+  public void youLose()
+  {
+    statePause();
+    menuActive = menuLose;
+    menuActive.SetActive(true);
+  }
+}
