7ff5931f9e68bf5e8398e72e40fa90d3978af5f5
AngeliqueHannagan_Game_Dev <ange.hannagan@gmail.com>
2026-03-13 16:01:59 -0400

    fixed error

--- Game_Dev_Prototype/Assets/Scripts/IDamage.cs
+++ Game_Dev_Prototype/Assets/Scripts/IDamage.cs
@@ -6,5 +6,5 @@ public interface IDamage
 
     // Shack for 0.15 seconds with the magnitud of 0.2
 
-    GetComponentInChildren<cameraController>().shack(0.15f, 0.2f)
+    GetComponentInChildren<cameraController>().shack(0.15f, 0.2f);
 }
