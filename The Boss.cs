bb0c4c7b0f2baaeda792ef2efed6c2969dacc415
DJCool360 <fuzzyhead5555@gmail.com>
2026-03-24 20:44:49 -0400

    Add files via upload

---
+++ The Boss.cs
@@ -0,0 +1,153 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class BossEnemy : MonoBehaviour, IDamage
+{
+    private enemyai baseai;
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    [SerializeField] int FOV;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamPauseTime;
+
+    Color colorOrg;
+
+    float shootTimer;
+    float roamTimer;
+
+    float angleToPlayer;
+    float stoppingDistaOrig;
+
+    bool playerInTrigger;
+
+    Vector3 playerDir;
+    Vector3 startingPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrg = model.material.color;
+        gamemanager.instance.updateGameGoal(1);
+        stoppingDistaOrig = agent.stoppingDistance;
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (agent.stoppingDistance < 0.01f)
+            roamTimer += Time.deltaTime;
+
+        if (playerInTrigger && !canSeePlayer())
+        {
+            checkroam();
+        }
+        else if (!playerInTrigger)
+        {
+            checkroam();
+        }
+    }
+
+    void checkroam()
+    {
+        if (agent.remainingDistance < 0.01f && roamTimer >= roamPauseTime)
+        {
+            roam();
+        }
+    }
+
+    void roam()
+    {
+        roamTimer = 0;
+        agent.stoppingDistance = 0;
+        Vector3 ranPos = Random.insideUnitSphere * roamDist;
+        ranPos += startingPos;
+        UnityEngine.AI.NavMeshHit hit;
+        UnityEngine.AI.NavMesh.SamplePosition(ranPos, out hit, roamDist, 1);
+        agent.SetDestination(hit.position);
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = gamemanager.instance.player.transform.position - transform.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+        Debug.DrawRay(transform.position, playerDir);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, playerDir, out hit))
+        {
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gamemanager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                    faceTarget();
+                if (shootTimer >= shootRate)
+                {
+                    shoot();
+                }
+                agent.stoppingDistance = stoppingDistaOrig;
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+
+        }
+    }
+
+    void onTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            agent.stoppingDistance = 0;
+
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+        if (HP <= 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashred());
+        }
+    }
+
+    IEnumerator flashred()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrg;
+    }
+}
