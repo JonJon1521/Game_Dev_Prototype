cb6f02e1cde54965cd8e71c85fe46b5e0aa9178e
domynicneal-design <domynicneal@gmail.com>
2026-04-19 21:14:13 -0400

    end goal debugging

--- Game_Dev_Prototype/Assets/Scripts/Endgoal.cs
+++ Game_Dev_Prototype/Assets/Scripts/Endgoal.cs
@@ -2,23 +2,11 @@ using UnityEngine;
 
 public class Endgoal : MonoBehaviour
 {
-    int requiredCollectibles;
-    int amount;
     bool playerInRange;
-
-    void Start()
-    {
-        requiredCollectibles =
-            Object.FindObjectsByType<Collectible>(FindObjectsSortMode.None).Length;
-
-        gamemanager.instance.SetRequiredCollectibles(requiredCollectibles);
-    }
 
     void Update()
     {
-        if (!playerInRange) return;
-
-        if (Input.GetKeyDown(KeyCode.L))
+        if (playerInRange && Input.GetKeyDown(KeyCode.L))
         {
             if (gamemanager.instance.IsGoalUnlocked())
             {
@@ -31,16 +19,15 @@ public class Endgoal : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             playerInRange = true;
     }
 
-    private void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
             playerInRange = false;
     }
-
 }
